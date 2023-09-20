3a653a515b580235df6a6110e548846fbbc7c718
mimvahedi <mimvhd@pm.me>
2023-09-20 14:03:53 +0330

    add follow button in popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,8 @@
-window.browser = (function() {
+window.browser = (function () {
   return window.msBrowser || window.browser || window.chrome;
 })();
-if(!!document.querySelector("a[href^='/process/rate_post/'][href$='2']")){
-  document.querySelector(".toggleLike")
+if (!!document.querySelector("a[href^='/process/rate_post/'][href$='2']")) {
+  document.querySelector("#toggleLike")
 }
 document.querySelector("#toggleLike").onclick = () => {
   browser.tabs.executeScript({ file: "/inject/enableRateButtons.js" });
@@ -23,3 +23,19 @@ document.querySelector("#showAds").onclick = () => {
     code: `.post[style] {display: var(--display, block) !important;--theme-color: red;}`,
   });
 };
+
+async function getCurrentTab() {
+  let [tab] = await browser.tabs.query({ active: true, lastFocusedWindow: true });
+  return tab;
+}
+(async () => {
+  let tab = await getCurrentTab();
+  let url = new URL(tab.url)
+  document.querySelector("#follow").addEventListener("click", () => {
+    browser.tabs.create({
+      url: `https://blog.ir/panel/-/followed_blogs?follow=${url.host}`
+    })
+  })
+  if (url.href !== "https://blog.ir/changes")
+    document.querySelector("#showAds").style.display = "none"
+})()
