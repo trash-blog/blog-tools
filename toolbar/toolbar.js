0a8f9ef5eeef807c1c1e3cb42962822515ed9869
mimvahedi <mimvhd@pm.me>
2024-01-08 15:29:01 +0330

    add toolbar

---
+++ toolbar/toolbar.js
@@ -0,0 +1,69 @@
+window.customElements.define("rate-box", RateElement);
+
+let isBlog = document.querySelector("meta[name=generator]").content === "blog.ir"
+let buttons = []
+
+let toolbar = document.createElement("div")
+toolbar.classList.add("toolbar")
+document.body.prepend(toolbar)
+
+
+
+if (document.querySelector("a[href^='/process/rate_post/'][href$='2']") && !(document.querySelector("a[href^='/process/rate_post/'][href$='1']"))) {
+    buttons.push({
+        "name": "toggleLike",
+        "label": "toggle rate",
+        "icon": "👍",
+        "fn": () => {
+            document.body.append(document.createElement("rate-box"))
+        }
+    })
+}
+
+
+buttons.push({
+    "name": "toggleComment",
+    "label": "toggle cm",
+    "icon": "💬",
+    "js": "/inject/bcomForm.js",
+    "css": "/inject/enableRateButtons.css"
+})
+
+buttons.push({
+    "name": "follow",
+    "label": "follow",
+    "icon": "➕",
+    "type": "link",
+    "href": `https://blog.ir/panel/-/followed_blogs?follow=${window.location.origin}`
+})
+
+if (isBlog) {
+    for (let btn of buttons) {
+        let type = btn.type == "link" ? "a" : "button"
+        let b = document.createElement(type)
+        b.classList.add("toolbar__btn")
+        b.id = b.name
+        if (btn.js) b.addEventListener("click", () => { injectScript(btn.js) })
+        if (btn.css) injectStyle(btn.css)
+        if (btn.fn) b.addEventListener("click", btn.fn)
+        if (btn.href) b.href = btn.href
+        b.title = b.label
+        b.textContent = btn.icon
+        toolbar.append(b)
+    }
+}
+
+function injectScript(src) {
+    let el = document.body.appendChild(document.createElement("script"));
+    el.type = "text/javascript";
+    el.src = browser.runtime.getURL(src)
+    document.head.appendChild(el)
+}
+
+function injectStyle(src) {
+    let el = document.body.appendChild(document.createElement("link"));
+    el.rel = "stylesheet"
+    el.type = "text/css";
+    el.href = browser.runtime.getURL(src)
+    document.head.appendChild(el)
+}
---
+++ toolbar/tools/enableRateButtons.js
@@ -0,0 +1,70 @@
+class RateElement extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+    const style = document.createElement("style");
+    style.textContent = `  .trash-rate-down{
+      position: fixed;
+      left: 5rem;
+      bottom: 1rem;
+      background: #018a87;
+      line-height: 1;
+      padding: 0.5em;
+      color: #fff;
+      font-weight: bold;
+      border-bottom: 4px solid #0006;
+      text-decoration: navajowhite;
+      border-radius: 6px;
+    }
+    .trash-rate-down::after {
+      content: attr(data-text);
+    }
+    .trash-rate-down:hover,.trash-rate-down:focus {
+      background: #015351;
+    }`
+    shadow.append(style)
+
+    let rateUp = document.querySelector(
+      "a[href^='/process/rate_post/'][href$='2']"
+    );
+    let rateDown = document.querySelector(
+      "a[href^='/process/rate_post/'][href$='1']"
+    );
+    if (rateUp && !rateDown) {
+      let a = document.createElement("a");
+      let url = rateUp.href.slice(NaN, -1) + "1";
+      a.classList.add("trash-rate-down");
+      a.textContent = "مخالف"
+      a.href = url
+      shadow.appendChild(a);
+      a.addEventListener("click", async (e) => {
+        e.preventDefault()
+        this.send_xhr(url,
+          (req) => {
+            const [upRates, downRates] = eval("(" + req.responseText + ")");
+            a.dataset.text = " (" + downRates + ")";
+          },
+          (req) => {
+            a.dataset.text = " خطا! ";
+            console.log(req)
+          },
+        )
+      })
+    }
+
+  }
+
+  send_xhr(url, onSuccess, onError) {
+    const request = new XMLHttpRequest();
+    request.open("GET", url),
+      request.setRequestHeader("X-Requested-With", "XMLHttpRequest"),
+      request.onreadystatechange = function () {
+        4 == request.readyState &&
+          (200 != request.status && 304 != request.status ? onError && onError(request) : onSuccess && onSuccess(request));
+      },
+
+      4 != request.readyState && request.send(null);
+  }
+}
